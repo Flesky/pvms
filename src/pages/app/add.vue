edee28aa8d50c85d9541a689c9fe050d071a89f1
Baron <qbmrodrigo@gmail.com>
2023-06-26 09:53:47 +0800

    File upload functionality

--- src/pages/app/add.vue
+++ src/pages/app/add.vue
@@ -20,13 +20,26 @@ const { loading, error, run } = useRequest(() => axios.post('/voucher-multiple',
     message.success('Voucher added')
   },
 })
+
+const fileList = ref([])
+const { loading: uploading, run: upload } = useRequest(() => {
+  const formData = new FormData()
+  formData.append('file', fileList.value[0].file)
+  return axios.post('/voucher-file', formData)
+}, {
+  manual: true,
+  onSuccess: () => {
+    fileList.value = []
+    message.success('Voucher/s added')
+  },
+})
 </script>
 
 <template>
   <div class="w-full p-4">
     <n-card header-style="padding-bottom: 8px;" title="Add voucher">
       <n-tabs size="large" type="line">
-        <n-tab-pane name="Single">
+        <n-tab-pane name="Manual Input">
           <n-form label-placement="left" :label-width="150">
             <n-form-item label="Value">
               <n-select v-model:value="formValue.value" class="max-w-xs" filterable :options="valueOptions" placeholder="" tag />
@@ -46,11 +59,26 @@ const { loading, error, run } = useRequest(() => axios.post('/voucher-multiple',
         </n-tab-pane>
 
         <n-tab-pane name="File Upload">
-          <n-upload :default-upload="false">
-            <n-button>
-              Upload file
-            </n-button>
-          </n-upload>
+          <n-space vertical>
+            <n-upload
+              v-model:file-list="fileList"
+              :max="1"
+            >
+              <n-upload-dragger class="flex flex-col items-center">
+                <n-icon class="mb-4" :depth="3" size="48">
+                  <i-archive />
+                </n-icon>
+                <n-text style="font-size: 16px">
+                  Click or drag file to this area to upload
+                </n-text>
+              </n-upload-dragger>
+            </n-upload>
+            <n-collapse-transition :collapsed="!!fileList.length">
+              <n-button :loading="uploading" type="primary" @click="upload">
+                Upload
+              </n-button>
+            </n-collapse-transition>
+          </n-space>
         </n-tab-pane>
       </n-tabs>
     </n-card>
