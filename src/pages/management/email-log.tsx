8e4a15e475e87adc25144297cddbba47e9737566
Baron <49512397+Flesky@users.noreply.github.com>
2024-06-24 21:12:22 +0800

    Update email-log.tsx

--- src/pages/management/email-log.tsx
+++ src/pages/management/email-log.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueries, useQueryClient } from '@tanstack/react-query'
-import { Box, Button, Fieldset, Group, NumberInput, Stack, Switch } from '@mantine/core'
+import { Box, Button, Fieldset, Group, Select, Stack, Switch } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { notifications } from '@mantine/notifications'
 import api from '@/utils/api.ts'
@@ -20,6 +20,26 @@ interface EmailConfiguration extends Result {
   configuration_description: string
 }
 
+const INTERVAL_VALUES = [
+  { value: '1', label: '1 minute' },
+  { value: '2', label: '2 minutes' },
+  { value: '3', label: '3 minutes' },
+  { value: '5', label: '5 minutes' },
+  { value: '10', label: '10 minutes' },
+  { value: '15', label: '15 minutes' },
+  { value: '20', label: '20 minutes' },
+  { value: '30', label: '30 minutes' },
+  { value: '45', label: '45 minutes' },
+  { value: '60', label: '1 hour' },
+  { value: '90', label: '1.5 hours' },
+  { value: '120', label: '2 hours' },
+  { value: '180', label: '3 hours' },
+  { value: '240', label: '4 hours' },
+  { value: '360', label: '6 hours' },
+  { value: '540', label: '9 hours' },
+  { value: '720', label: '12 hours' },
+]
+
 export default function EmailLog() {
   const queryClient = useQueryClient()
 
@@ -30,12 +50,6 @@ export default function EmailLog() {
 
       if (values.alert_email_toggle !== previous.alert_email_toggle)
         mutate({ id: 1, value: Number(values.alert_email_toggle), message: `Automatic email alerts ${values.alert_email_toggle ? 'enabled' : 'disabled'}` })
-    },
-    validate: {
-      alert_email_interval: (value) => {
-        if (value < 1 || value > 24)
-          return 'Interval must be between 1 and 24 hours'
-      },
     },
     validateInputOnBlur: true,
     enhanceGetInputProps: () => ({
@@ -55,7 +69,7 @@ export default function EmailLog() {
           const results = (await api.get('alertEmailConfigurationPrivate').json<GetAllResponse<EmailConfiguration>>()).results
           form.initialize({
             alert_email_toggle: Boolean(results[0].configuration_value),
-            alert_email_interval: results[1].configuration_value,
+            alert_email_interval: String(results[1].configuration_value),
           })
           return results
         },
@@ -88,10 +102,18 @@ export default function EmailLog() {
         <Stack>
           <Switch label="Automatic email alerts" {...form.getInputProps('alert_email_toggle', { type: 'checkbox' })} />
           <Group align="end">
-            <NumberInput min={1} max={24} label="Email alert interval (hours)" {...form.getInputProps('alert_email_interval')} />
+            <Select
+              data={INTERVAL_VALUES}
+              label="Email alert interval"
+              allowDeselect={false}
+              {...form.getInputProps('alert_email_interval')}
+            >
+            </Select>
             <Button
               onClick={() => mutate(
-                { id: 2, value: form.values.alert_email_interval, message: `Email alert interval set to ${form.values.alert_email_interval} hours` },
+                { id: 2, value: form.values.alert_email_interval, message: `Email alert interval set to ${
+                  INTERVAL_VALUES.find(({ value }) => value === form.values.alert_email_interval)?.label
+                }` },
               )}
               disabled={isPending || isSavePending}
             >
